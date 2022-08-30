4f1f0e0159ec11bc81fdd0a673efd46edc231cfc
hafsa <hafsah_kh_2000@hotmail.com>
2022-08-31 01:21:49 +0300

    make the map function

--- JavaScriptTask/JsTask1/arrayMethods.js
+++ JavaScriptTask/JsTask1/arrayMethods.js
@@ -12,59 +12,98 @@
  * @param {callback} Callback
  */
 
- function myFilter(arr, callback) {
-    let array = [];
-  
-    for (let i = 0; i < arr.length; i++) {
-      if (callback(arr[i])) array.push(arr[i]);
-    }
-    return array;
+function myFilter(arr, callback) {
+  let array = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i])) array.push(arr[i]);
   }
-  
-  /**
-   * an Array prototype function that returns a filtered list based on the provied callback function
-   * @param {Array} thisArg
-   * @param {givenCallback} callback
-   */
-  
-  Array.prototype.myFilterFunc = function myFilterFunc(callback, thisArg) {
-    let array = [];
-  
-    for (let i = 0; i < this.length; i++) {
-      if (callback.call(thisArg, this[i], i, this)) array.push(this[i]);
-    }
-    return array;
-  };
-  
-  /**
-   * example to try the functions
-   */
-  
-  const items = [
-    {
-      name: "book",
-      price: 50,
-      id: "12fa21",
-    },
-    {
-      name: "book2",
-      price: 10,
-      id: "12fa21",
-    },
-    {
-      name: "book2",
-      price: 20,
-      id: "12fa21",
-    },
-    {
-      name: "book2",
-      price: 30,
-      id: "12fa21",
-    },
-  ];
-  
-  const itemsAsPriceMoreThan20 = items.myFilterFunc(({ price }) => price > 20);
-  console.log(itemsAsPriceMoreThan20);
-  
-  console.log(myFilter(items, ({ price }) => price > 20));
-  
+  return array;
+}
+
+/**
+ * an Array prototype function that returns a filtered list based on the provided callback function
+ * @param {Array} thisArg
+ * @param {givenCallback} callback
+ */
+
+Array.prototype.myFilterFunc = function myFilterFunc(callback, thisArg) {
+  let array = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (callback.call(thisArg, this[i], i, this)) array.push(this[i]);
+  }
+  return array;
+};
+
+/**
+ * a function that returns a mapped list based on the provided function
+ * @param {Array} arr
+ * @param {callback} Callback
+ */
+
+function myMap(arr, callback) {
+  let array = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    array.push(callback(arr[i]));
+  }
+  return array;
+}
+
+/**
+ * an Array prototype function that returns a mapped list based on the provided function
+ * @param {givenCallback} callback
+ * @param {Array} thisArg optinal
+ */
+Array.prototype.myMapFunc = function (callback, thisArg) {
+  let array = [];
+
+  for (let i = 0; i < this.length; i++) {
+    array.push(callback.call(thisArg, this[i], i, this));
+  }
+  return array;
+};
+
+/**
+ * example to try the functions
+ */
+
+const items = [
+  {
+    name: "book",
+    price: 50,
+    id: "12fa21",
+  },
+  {
+    name: "book2",
+    price: 10,
+    id: "12fa21",
+  },
+  {
+    name: "book2",
+    price: 20,
+    id: "12fa21",
+  },
+  {
+    name: "book2",
+    price: 30,
+    id: "12fa21",
+  },
+];
+
+const itemsAsPriceMoreThan20 = items.myFilterFunc(({ price }) => price > 20);
+console.log(itemsAsPriceMoreThan20);
+
+console.log(myFilter(items, ({ price }) => price > 20));
+
+const appliedDiscountItems = items.myMapFunc((item) =>
+  item.price > 20 ? { ...item, price: item.price * 0.99 } : item
+);
+
+console.log(appliedDiscountItems);
+console.log(
+  myMap(items, (item) =>
+    item.price > 20 ? { ...item, price: item.price * 0.99 } : item
+  )
+);
